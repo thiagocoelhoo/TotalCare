9678e486efbcd839019969438c7a4d508cffeec9
Thiago Coelho <thiago.coelho158@gmail.com>
2024-12-05 16:44:00 -0300

    Initial commit

---
+++ src/main/java/br/edu/ufersa/pw/totalCare/TotalCareApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.pw.totalCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufersa/pw/totalCare/TotalCareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.pw.totalCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
