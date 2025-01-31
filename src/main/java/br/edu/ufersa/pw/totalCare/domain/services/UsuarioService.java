2d766c11ad28d0821bfc16a380c5170179a426b1
Thiago Coelho <thiago.coelho158@gmail.com>
2025-01-31 18:39:14 -0300

    fix: substituir UsuarioDTO por UsuarioCreateDTO em metodo de criacao de usuario

--- src/main/java/br/edu/ufersa/pw/totalCare/domain/services/UsuarioService.java
+++ src/main/java/br/edu/ufersa/pw/totalCare/domain/services/UsuarioService.java
@@ -3,6 +3,7 @@ package br.edu.ufersa.pw.totalCare.domain.services;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import br.edu.ufersa.pw.totalCare.api.dtos.UsuarioCreateDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -21,19 +22,9 @@ public class UsuarioService {
         return ResponseEntity.ok(usuarioRepository.findAll());
     }
 
-    public Usuario criarUsuario(UsuarioDTO usuarioDto){
-        Usuario usuario = new Usuario();
-        usuario.setEmail(usuarioDto.getEmail());
-        usuario.setSenha(usuarioDto.getSenha());
+    public Usuario criarUsuario(UsuarioCreateDTO usuarioDTO){
+        Usuario usuario = new Usuario(usuarioDTO);
         usuario.setDataCadastro(LocalDateTime.now());
-        usuario.setPerfil(null);
-
-        //TODO: FAZER A BUSCA DO PERFIL DEPOIS DE CRIAR A CRIAÇÃO DO PERFIL
-        
-        // Perfil perfil = perfilRepository.findById(usuarioDto.getPerfil().getId())
-        // .orElse(new Perfil("USUARIO", null, null, null, null, null, null, null, null));
-
-        // usuario.setPerfil(perfil);
 
         return usuarioRepository.save(usuario);
     }
